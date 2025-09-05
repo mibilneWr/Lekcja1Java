85394b9f85beab7d350005020cacec0a57b2f627
mibilneWr <wiktor.rojek@uczen.mechaniktg.pl>
2025-09-05 11:43:06 +0200

    Klasa Osoba

---
+++ src/Osoba.java
@@ -0,0 +1,33 @@
+public abstract class Osoba {
+    //abstract - nie można utworzyć obiketu tej klasy
+    private String imie, nazwisko;
+
+    public Osoba(String nazwisko, String imie) {
+        this.nazwisko = nazwisko;
+        this.imie = imie;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    @Override
+    public String toString() {
+        return "Osoba{" +
+                "imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                '}';
+    }
+}
